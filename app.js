15c90e3f268532a49c5a5d342cf64417d4a8421b
fadly <fadlydestriana@gmail.com>
2022-09-25 15:26:37 +0700

    adding Route object as a middleware and routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const birds = require("./birds");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/birds', birds);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +39,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- birds.js
+++ birds.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 
 // middleware that is specific  to this router
@@ -6,3 +6,15 @@ router.use((req, res, next) => {
     console.log("Time: ", Date.now());
     next();
 })
+
+// define the home page route
+router.get("/", (req, res) => {
+    res.send("Birds home page");
+})
+
+// define the about route
+router.get("/about", (req, res) => {
+    res.send("About birds");
+})
+
+module.exports = router;
